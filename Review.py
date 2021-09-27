25eb73148f75e4e288650554161c49748782fd86
BriggsFisher <18WBFI@queensu.ca>
2021-09-27 16:06:24 -0400

    Removing User from Review.py

--- Review.py
+++ Review.py
@@ -6,15 +6,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
 """
 Lays out the attributes for reviews that users can place on products
 with a comment and a rating
