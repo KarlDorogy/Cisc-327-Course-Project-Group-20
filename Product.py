b861be519426ce896f5246a08589b02ef7b0785e
To-m-L <tom8731@gmail.com>
2021-09-27 14:39:13 -0400

    added primray_key attribute to id

--- Product.py
+++ Product.py
@@ -10,7 +10,7 @@ class Product(db.Model):
    price = db.Column(db.Integer) # The price of the product. The value must be an integer.
    brand = db.Column(db.String(100), unique=True, nullable = False) # The brand name of the product. Must be unique and can not be null
    item_type = db.Column(db.String(100), unique=False, nullable=False) # The item type of the product. Can not be null
-   id = db.Column(db.Integer) # The id of the product. Used to identify the product in other entities.
+   id = db.Column(db.Integer, primary_key=True) # The id of the product. Used to identify the product in other entities.
    quantity = db.Column(db.Integer) # The amount of the product that exists. The value must be an integer.
    attributes = db.Column(db.String(1000), unique=False, nullable=True) # Special attributes of the given product (i.e phones have storage).
 
