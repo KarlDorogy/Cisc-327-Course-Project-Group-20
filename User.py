495a5a55df46e018ae04f9d8e51d3f843667823e
DouglasChafee <douglas.chafee@gmail.com>
2021-09-27 15:21:04 -0400

    Added comments and addressed Karl's comments

--- User.py
+++ User.py
@@ -7,16 +7,22 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    # Sets primary key so we can map things to table
     id = db.Column(db.Integer, primary_key=True)
+    # The alias displayed on transactions
     username = db.Column(db.String(80), unique=True, nullable=False)
+    # Used to verify user
     email = db.Column(db.String(120), unique=True, nullable=False)
-    verified = db.Column(db.Bool, unique=True, nullable=False)
+    # Only verified users can make reviews
+    verified = db.Column(db.Bool, unique=False, nullable=False)
+    # Shows user balance, will be used for transactions
     wallet = db.Column(db.Float, unique=True, nullable=False)
-    shopping_cart = db.Column(db.Integer, unique=True, nullable=True)
+    # Tracks all prodocts the user is buying
+    shopping_cart = db.Column(db.List, unique=True, nullable=True)
+    # Tracks all products the user wants to buy
     wish_list = db.Column(db.Integer, unique=True, nullable=True)
+    # User address that products are shipped to
     shipping_address = db.Column(db.String(200), unique=True, nullable=False)
-
-
 
     def __repr__(self):
         return '<User %r>' % self.username
