3add8c0fd73b0375c776de9ac0f5ee32011191f2
70972627 <18kwd1@queensu.ca>
2021-09-27 20:58:03 -0400

    Added Pull Request Comments and Updated Transactions.py

--- Transaction.py
+++ Transaction.py
@@ -14,15 +14,18 @@ class Transaction(db.Model):
     # Sets up primary key id for each succesful transaction through Qbay
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     # Sets a seller id for the seller of a product with each transaction
-    seller_id = db.Column(db.Integer, unique=False, nullable=False)
+    seller_id = db.Column(db.Integer, unique=False, nullable=True)
     # Sets a buyer id for the buyer of a product with each transaction
-    buyer_id = db.Column(db.Integer, unique=False, nullable=False)
+    buyer_id = db.Column(db.Integer, unique=False, nullable=True)
     # The amount or price of a the transaction between users
     amount = db.Column(db.Float, unique=False, nullable=False)
     # Sets a timestamp of a transaction
     timestamp = db.Column(db.String(100), unique=False, nullable=False)
     # The product id that was associated with the transaction
-    product_id = db.Column(db.Float, unique=False, nullable=False)
+    product_id = db.Column(db.Integer, unique=False, nullable=False)
+    # Represents if a transaction is between two users
+    # Or if the transaction is a addition to a user's balance
+    balance_transaction = db.Column(db.Boolean, unique=False, nullable=False)
 
     def __repr__(self):
         return '<Transaction ID: %r>' % self.id
@@ -31,3 +34,4 @@ class Transaction(db.Model):
         return '<Transaction Amount: %r>' % self.amount
         return '<Transaction Timestamp: %r>' % self.timestamp
         return '<Transaction Product ID: %r>' % self.product_id
+        return '<Addition to a Balance?: %r>' % self.balance_transaction
