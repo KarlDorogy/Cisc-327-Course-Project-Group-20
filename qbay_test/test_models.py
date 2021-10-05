92ee5e1009a51560e5938fa01615c2be728c06b5
70972627 <18kwd1@queensu.ca>
2021-10-05 18:54:46 -0400

    Updated Models Database Atrtributes

--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -8,7 +8,7 @@ def test_r1_7_user_register():
 
     assert register('u0', 'test0@test.com', '123456') is True
     assert register('u0', 'test1@test.com', '123456') is True
-    assert register('u1', 'test0@test.com', '123456') is False, "Oh no! This assertion failed!"
+    assert register('u1', 'test0@test.com', '123456') is False
 
 
 def test_r2_1_login():
