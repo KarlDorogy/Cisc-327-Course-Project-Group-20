87895340d218da084032bf2e84868cb054ade7d1
70972627 <18kwd1@queensu.ca>
2021-11-03 17:53:05 -0400

    Added r1_1 testing

--- qbay_test/frontend/test_registration.py
+++ qbay_test/frontend/test_registration.py
@@ -11,53 +11,62 @@ This file defines all integration tests for the frontend homepage.
 
 class FrontEndHomePageTest(BaseCase):
 
-    def test_register_success(self, *_):
+    def test_register_frontend_r1_1(self, *_):
         """
-        This is a sample front end unit test to login to home page
-        and verify if the tickets are correctly listed.
+        This is BlackBox Input Partition Test for R1-1.
+        Both the email and password cannot be empty
         """
 
-        # open register page
+        # P1: email empty
         self.open(base_url + '/register')
-        # fill email and password
+        self.type("#email", " ")
+        self.type("#name", "u0")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Registration Failed.", "#message")
+
+        # P2 password empty
+        self.open(base_url + '/register')
+        self.type("#email", "test2@email.com")
+        self.type("#name", "u0")
+        self.type("#password", " ")
+        self.type("#password2", " ")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Registration Failed.", "#message")
+
+        # P3 both email & password empty
+        self.open(base_url + '/register')
+        self.type("#email", " ")
+        self.type("#name", "u0")
+        self.type("#password", " ")
+        self.type("#password2", " ")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Registration Failed.", "#message")
+
+        # P4 both email & password filled
+        self.open(base_url + '/register')
         self.type("#email", "test0@test.com")
         self.type("#name", "u0")
         self.type("#password", "@Password")
         self.type("#password2", "@Password")
-        # click enter button
-        self.click('input[type="submit"]')
-        
-        # open login page
-        self.open(base_url + '/login')
-        # fill email and password
-        self.type("#email", "test0@test.com")
-        self.type("#password", "@Password")
-        # click enter button
         self.click('input[type="submit"]')
 
-        # after clicking on the browser (the line above)
-        # the front-end code is activated
-        # and tries to call get_user function.
-        # The get_user function is supposed to read data from database
-        # and return the value. However, here we only want to test the
-        # front-end, without running the backend logics.
-        # so we patch the backend to return a specific user instance,
-        # rather than running that program. (see @ annotations above)
-
-        # open home page
-        self.open(base_url)
-        # test if the page loads correctly
-        self.assert_element("#welcome-header")
-        self.assert_text("Welcome u0!", "#welcome-header")
-        # other available APIs
-
+        # If P4 registration was successful user is redirected to login page
+        # with a messege of "Please login"
+        self.assert_element("#message")
+        self.assert_text('Please login', "#message")
+    
     def test_login_success(self, *_):
         """
         This is a sample front end unit test to login to home page
         and verify if the tickets are correctly listed.
         """
-        # register user for login frontend login test to use
-        register("Karl Dorogy", "test1@test.com", "@Password")
+        # register new user for frontend login test to use
+        register("u1", "test1@test.com", "@Password")
 
         # open login page
         self.open(base_url + '/login')
@@ -80,5 +89,5 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url)
         # test if the page loads correctly
         self.assert_element("#welcome-header")
-        self.assert_text("Welcome Karl Dorogy!", "#welcome-header")
+        self.assert_text("Welcome u1!", "#welcome-header")
         # other available APIs
