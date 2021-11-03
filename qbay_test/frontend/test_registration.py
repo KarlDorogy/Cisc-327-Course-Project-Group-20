2683157f31e33f5ec2bcd84fe7c6cad162806729
70972627 <18kwd1@queensu.ca>
2021-11-03 19:04:36 -0400

    Added r1_1,6,8 frontend testing

--- qbay_test/frontend/test_registration.py
+++ qbay_test/frontend/test_registration.py
@@ -29,8 +29,8 @@ class FrontEndHomePageTest(BaseCase):
 
         # P2 password empty
         self.open(base_url + '/register')
-        self.type("#email", "test2@email.com")
-        self.type("#name", "u0")
+        self.type("#email", "partition2@r11.com")
+        self.type("#name", "u2")
         self.type("#password", " ")
         self.type("#password2", " ")
         self.click('input[type="submit"]')
@@ -40,7 +40,7 @@ class FrontEndHomePageTest(BaseCase):
         # P3 both email & password empty
         self.open(base_url + '/register')
         self.type("#email", " ")
-        self.type("#name", "u0")
+        self.type("#name", "u3")
         self.type("#password", " ")
         self.type("#password2", " ")
         self.click('input[type="submit"]')
@@ -49,8 +49,8 @@ class FrontEndHomePageTest(BaseCase):
 
         # P4 both email & password filled
         self.open(base_url + '/register')
-        self.type("#email", "test0@test.com")
-        self.type("#name", "u0")
+        self.type("#email", "partition4@r11.com")
+        self.type("#name", "u4")
         self.type("#password", "@Password")
         self.type("#password2", "@Password")
         self.click('input[type="submit"]')
@@ -59,6 +59,89 @@ class FrontEndHomePageTest(BaseCase):
         # with a messege of "Please login"
         self.assert_element("#message")
         self.assert_text('Please login', "#message")
+
+    def test_register_frontend_r1_6(self, *_):
+        """
+        This is BlackBox Input Boundary/Paritioning Testing 
+        for R1-6. User name has to be longer than 2 characters 
+        and less than 20 characters.
+        """
+
+        # # T1: less than 2 characters
+        self.open(base_url + '/register')
+        self.type("#email", "test1@r16.com")
+        self.type("#name", "u")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Registration Failed.", "#message")
+
+        # # T2: more than 2 characters less than 20 characters
+        self.open(base_url + '/register')
+        self.type("#email", "test2@r16.com")
+        self.type("#name", "user1")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        # If P4 registration was successful user is redirected to login page
+        # with a messege of "Please login"
+        self.assert_element("#message")
+        self.assert_text('Please login', "#message")
+
+        # # T3: more than 20 characters
+        self.open(base_url + '/register')
+        self.type("#email", "test3@r16.com")
+        self.type("#name", "thisusernameismorethan")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Registration Failed.", "#message")
+
+        # # T4: 20 characters
+        self.open(base_url + '/register')
+        self.type("#email", "test4@r16.com")
+        self.type("#name", "thisusernamesisexact")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text('Please login', "#message")
+
+        # # T5: 2 characters
+        self.open(base_url + '/register')
+        self.type("#email", "test5@r16.com")
+        self.type("#name", "u2")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text('Please login', "#message")
+
+    def test_register_frontend_r1_8(self, *_):
+        """
+        This is BlackBox Functionality Testing for R1-8.
+        Shipping addrss must be empty (only) in the case when a
+        user just registered a new account.
+        """
+
+        # T1: new user account registration
+        self.open(base_url + '/register')
+        self.type("#email", "test1@r18.com")
+        self.type("#name", "u0")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        self.click('input[type="submit"]')
+
+        self.open(base_url + '/login')
+        self.type("#email", "test1@r18.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        self.open(base_url)
+        self.assert_element("#shipping-header")
+        self.assert_text("None", "#shipping-header")
     
     def test_login_success(self, *_):
         """
