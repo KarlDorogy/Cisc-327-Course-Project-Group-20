31a64b90544d470e186edad007704c7991eb0cc8
70972627 <18kwd1@queensu.ca>
2021-11-03 13:50:35 -0400

    frontend user page testing

--- qbay_test/frontend/test_registration.py
+++ qbay_test/frontend/test_registration.py
@@ -2,7 +2,7 @@ from seleniumbase import BaseCase
 
 from qbay_test.conftest import base_url
 from unittest.mock import patch
-from qbay.models import User
+from qbay.models import *
 
 """
 This file defines all integration tests for the frontend homepage.
@@ -11,16 +11,27 @@ This file defines all integration tests for the frontend homepage.
 
 class FrontEndHomePageTest(BaseCase):
 
-    def test_login_success(self, *_):
+    def test_register_success(self, *_):
         """
         This is a sample front end unit test to login to home page
         and verify if the tickets are correctly listed.
         """
-        # open login page
-        self.open(base_url + '/login')
+
+        # open register page
+        self.open(base_url + '/register')
         # fill email and password
         self.type("#email", "test0@test.com")
-        self.type("#password", "123456")
+        self.type("#name", "u0")
+        self.type("#password", "@Password")
+        self.type("#password2", "@Password")
+        # click enter button
+        self.click('input[type="submit"]')
+        
+        #open login page
+        self.open(base_url + '/login')
+        #fill email and password
+        self.type("#email", "test0@test.com")
+        self.type("#password", "@Password")
         # click enter button
         self.click('input[type="submit"]')
 
@@ -37,5 +48,38 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url)
         # test if the page loads correctly
         self.assert_element("#welcome-header")
-        self.assert_text("Welcome u0 !", "#welcome-header")
+        self.assert_text("Welcome u0!", "#welcome-header")
+        # other available APIs
+
+
+    def test_login_success(self, *_):
+        """
+        This is a sample front end unit test to login to home page
+        and verify if the tickets are correctly listed.
+        """
+        # register user for login frontend login test to use
+        register("Karl Dorogy", "test1@test.com", "@Password")
+
+        # open login page
+        self.open(base_url + '/login')
+        # fill email and password
+        self.type("#email", "test1@test.com")
+        self.type("#password", "@Password")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # after clicking on the browser (the line above)
+        # the front-end code is activated
+        # and tries to call get_user function.
+        # The get_user function is supposed to read data from database
+        # and return the value. However, here we only want to test the
+        # front-end, without running the backend logics.
+        # so we patch the backend to return a specific user instance,
+        # rather than running that program. (see @ annotations above)
+
+        # open home page
+        self.open(base_url)
+        # test if the page loads correctly
+        self.assert_element("#welcome-header")
+        self.assert_text("Welcome Karl Dorogy!", "#welcome-header")
         # other available APIs
