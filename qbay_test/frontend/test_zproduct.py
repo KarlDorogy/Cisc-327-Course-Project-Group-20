76642e5f521c9b64034e044bf89d8e2e9de15454
To-m-L <tom8731@gmail.com>
2021-11-04 01:05:47 -0400

    Fixed push issues

--- qbay_test/frontend/test_zproduct.py
+++ qbay_test/frontend/test_zproduct.py
@@ -6,9 +6,159 @@ from qbay.models import User
 
 class FrontEndCreateProductTest(BaseCase):
 
-    def listing_creation_success(self, *_):
+
+    def test_create_product_r4_3(self, *_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        #R4-3 The description of the product can be arbitrary characters, with a minimum length of 20 characters and a maximum of 2000 characters.
+
+        #Input Boundary Testing
+        #Input Partitioning
+
+        # P1 The description of the product has to have a minimum length of 20 characters (Success)
+
         self.open(base_url + '/createproduct')
         self.type("#price", 1000)
-        self.type("#title", "Test Product")
-        self.type("#description", "This is a test description made to be at least twenty characters long")
+        self.type("#title", "iPhone One")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        # P1 The description of the product has to have a minimum length of 20 characters (Failed)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "iPhone Two")
+        self.type("#description", "short desc")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+        # P2 and a maximum of 2000 characters. (Success)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "iPhone Three")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        # P2 and a maximum of 2000 characters. (Failed)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "iPhone Four")
+        self.type("#description", """dkjalhdkljahdkljashdkahdjsha
+                         daljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhadha
+                         iodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaakd
+                         dkjalhdkljahdkljashdkahdjshadaljhdklsahdashdkahdahdal
+                         hkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxcjka
+                         hsdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljashdkahdj
+                         shadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwha
+                         dhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadha
+                         akddkjalhdkljahdkljashdkahdjshadaljhdklsahdashdkahdah
+                         dalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxc
+                         jkahsdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljashdka
+                         hdjshadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodg
+                         whadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjka
+                         dhaakddkjalhdkljahdkljashdkahdjshadaljhdklsahdashdkah
+                         dahdalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandkla
+                         nxcjkahsdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljash
+                         dkahdjshadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyu
+                         odgwhadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdlad
+                         jkadhaakddkjalhdkljahdkljashdkahdjshadaljhdklsahdashd
+                         kahdahdalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsand
+                         klanxcjkahsdkahdwiuahdksdladjkadhaakddkjalhdkljahdklj
+                         ashdkahdjshadaljhdklsahdashdkahdahdalhkdajkndkjasldha
+                         iyuodgwhadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksd
+                         ladjkadhaakddkjalhdkljahdkljashdkahdjshadaljhdklsahda
+                         shdkahdahdalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjls
+                         andklanxcjkahsdkahdwiuahdksdladjkadhaakddkjalhdkljahd
+                         kljashdkahdjshadaljhdklsahdashdkahdahdalhkdajkndkjasl
+                         dhaiyuodgwhadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahd
+                         ksdladjkadhaakddkjalhdkljahdkljashdkahdjshadaljhdklsa
+                         hdashdkahdahdalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwk
+                         jlsandklanxcjkahsdkahdwiuahdksdladjkadhaakddkjalhdklj
+                         ahdkljashdkahdjshadaljhdklsahdashdkahdahdalhkdajkndkj
+                         asldhaiyuodgwhadhaiodnbkadwkjlsandklanxcjkahsdkahdwiu
+                         ahdksdladjkadhaakddkjalhdkljahdkljashdkahdjshadaljhdk
+                         lsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhadhaiodnbka
+                         dwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaakddkjalhd
+                         kljahdkljashdkahdjshadaljhdklsahdashdkahdahdalhkdajkn
+                         dkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxcjkahsdkahd
+                         wiuahdksdladjkadhaakddkjalhdkljahdkljashdkahdjshadalj
+                         hdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhadhaiodn
+                         bkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaakddkja
+                         lhdkljahdkljashdkahdjshadaljhdklsahdashdkahdahdalhkda
+                         jkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxcjkahsdk
+                         ahdwiuahdksdladjkadhaakddkjalhdkljahdkljashdkahdjshad
+                         aljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhadhai
+                         odnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaakdd
+                         kjalhdkljahdkljashdkahdjshadaljhdklsahdashdkahdahdalh
+                         kdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxcjkah
+                         sdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljashdkahdjs
+                         hadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhad
+                         haiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaa
+                         kddkjalhdkljahdkljashdkahdjshadaljhdklsahdashdkahdahd
+                         alhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklanxcj
+                         kahsdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljashdkah
+                         djshadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgw
+                         hadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkad
+                         haakddkjalhdkljahdkljashdkahdjshadaljhdklsahdashdkahd
+                         ahdalhkdajkndkjasldhaiyuodgwhadhaiodnbkadwkjlsandklan
+                         xcjkahsdkahdwiuahdksdladjkadhaakddkjalhdkljahdkljashd
+                         kahdjshadaljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuo
+                         dgwhadhaiodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladj
+                         kadhaakd""")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+    def test_create_product_r4_5(self, *_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        #R4-5 Price has to be of range [10, 10000].
+
+        #Input Boundary Testing
+        #P1 Price has to be at least 10 (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "iPhone Five")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        #P1 Price has to be at least 10 (Failed)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 9)
+        self.type("#title", "iPhone Six")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+        #P2 Price has to be below 10000 (Success)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "iPhone Seven")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        #P2 Price has to be below 10000 (Failed)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 10001)
+        self.type("#title", "iPhone Eight")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
