16c10eb4818b4616f96bbc514b644bec4e38c1e1
To-m-L <tom8731@gmail.com>
2021-11-07 21:30:54 -0500

    Added more tests

--- qbay_test/frontend/test_zproduct.py
+++ qbay_test/frontend/test_zproduct.py
@@ -7,6 +7,98 @@ from qbay.models import User
 class FrontEndCreateProductTest(BaseCase):
 
 
+    def test_create_product_r4_1(self, *_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        #R4-1: The title of the product has to be alphanumeric-only, and space allowed only if it is not as prefix and suffix.
+
+        #Input Partitioning
+
+        #P1 The title of the product has to be alphanumeric-only (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "ProductR11")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        #P1 The title of the product has to be alphanumeric-only (Fail)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "%Beans")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+        #P2 space allowed only if it is not as prefix and suffix (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "Product R12")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        #P2 space allowed only if it is not as prefix and suffix (Fail)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", " ProductR13")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+        #P2 space allowed only if it is not as prefix and suffix (Fail)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "ProductR14 ")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+    def test_create_product_r4_2(self, *_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        #R4-2: The title of the product is no longer than 80 characters.
+
+        # Input Exhaustive
+
+        # Case 1 The title is less than 80 characters (Success)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "ProductR2")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        # Case 2 The title is more than 80 characters (Fail)
+
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "jkasdhakdhakldhasdhakljjdashjdajkdhkajsldhakljdhjsadnakjldhnwajkcjdgajsdkahjSSSSS")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+        # Case 3 The title is 80 characters (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 1000)
+        self.type("#title", "jkasdhakdhakldhasdhakljjdashjdajkdhkajsldhakljdhjsadnakjldhnwajkcjdgajsdkahjSSSS")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
     def test_create_product_r4_3(self, *_):
         #Logs in to a user before creating products
         self.open(base_url + '/login')
@@ -23,7 +115,7 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 1000)
-        self.type("#title", "iPhone One")
+        self.type("#title", "ProductR31")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#welcome-header")
@@ -32,7 +124,7 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 1000)
-        self.type("#title", "iPhone Two")
+        self.type("#title", "ProductR32")
         self.type("#description", "short desc")
         self.click('input[type="submit"]')
         self.assert_element("#message")
@@ -42,7 +134,7 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 1000)
-        self.type("#title", "iPhone Three")
+        self.type("#title", "ProductR33")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#welcome-header")
@@ -51,7 +143,7 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 1000)
-        self.type("#title", "iPhone Four")
+        self.type("#title", "ProductR34")
         self.type("#description", """dkjalhdkljahdkljashdkahdjsha
                          daljhdklsahdashdkahdahdalhkdajkndkjasldhaiyuodgwhadha
                          iodnbkadwkjlsandklanxcjkahsdkahdwiuahdksdladjkadhaakd
@@ -118,6 +210,41 @@ class FrontEndCreateProductTest(BaseCase):
         self.assert_element("#message")
         self.assert_text("Product Creation Failed", "#message")
 
+    def test_create_product_r4_4(self, *_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        # R4-4: Description has to be longer than the product's title.
+
+        # Input Exhaustive
+
+        # Case 1 The description is longer than the title (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "ProductR4")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        # Case 2 The description is shorter than the title (Fail)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "ProductR42andenoughcharacterstohavemorethantwentycharacters")
+        self.type("#description", "This is a description that is long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
+        # Case 3 The description is the same length as the title (Fail)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "ProductR43andenoughcharacterstohavemorethantwentycharacters")
+        self.type("#description", "ProductR43andenoughcharacterstohavemorethantwentycharacters")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+
     def test_create_product_r4_5(self, *_):
         #Logs in to a user before creating products
         self.open(base_url + '/login')
@@ -131,7 +258,7 @@ class FrontEndCreateProductTest(BaseCase):
         #P1 Price has to be at least 10 (Success)
         self.open(base_url + '/createproduct')
         self.type("#price", 11)
-        self.type("#title", "iPhone Five")
+        self.type("#title", "ProductR51")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#welcome-header")
@@ -139,7 +266,7 @@ class FrontEndCreateProductTest(BaseCase):
         #P1 Price has to be at least 10 (Failed)
         self.open(base_url + '/createproduct')
         self.type("#price", 9)
-        self.type("#title", "iPhone Six")
+        self.type("#title", "ProductR52")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#message")
@@ -149,7 +276,7 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 11)
-        self.type("#title", "iPhone Seven")
+        self.type("#title", "ProductR53")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#welcome-header")
@@ -158,7 +285,32 @@ class FrontEndCreateProductTest(BaseCase):
 
         self.open(base_url + '/createproduct')
         self.type("#price", 10001)
-        self.type("#title", "iPhone Eight")
+        self.type("#title", "ProductR54")
         self.type("#description", "This is a very long test description that is at least 20 characters long")
         self.click('input[type="submit"]')
         self.assert_element("#message")
+        self.assert_text("Product Creation Failed", "#message")
+
+    def test_create_product_r4_6(self,*_):
+        #Logs in to a user before creating products
+        self.open(base_url + '/login')
+        self.type("#email", "test4@r16.com")
+        self.type("#password", "@Password")
+        self.click('input[type="submit"]')
+
+        #R4-6: last_modified_date must be after 2021-01-02 and before 2025-01-02.
+
+        #Input Partitioning
+
+        #P1 The date must be after 2021-01-02 (Success)
+        self.open(base_url + '/createproduct')
+        self.type("#price", 11)
+        self.type("#title", "ProductR61")
+        self.type("#description", "This is a very long test description that is at least 20 characters long")
+        self.click('input[type="submit"]')
+        self.assert_element("#welcome-header")
+        
+
+
+
+    
